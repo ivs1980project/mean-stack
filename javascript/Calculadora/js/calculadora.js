bf9163c03b2f61b6ff16d4b63a5374ed254471a0
manana <manana>
2016-10-20 13:59:16 +0200

    Calculadora más ejemplos

--- javascript/Calculadora/js/calculadora.js
+++ javascript/Calculadora/js/calculadora.js
@@ -1,5 +1,31 @@
-function boton1() {
-    console.log("Has pulsado 1");
-    var valor = this.getvalue;
+var memoria;
+function boton(valor) {
     console.log(valor);
+    digitosDisplay(valor);
+}
+
+function digitosDisplay(valor){
+		document.getElementById("display").value += valor;
+}
+
+function clearDisplay(){
+        document.getElementById("display").value = null;
+}
+
+function calcular(){
+        var resul = eval(document.getElementById("display").value);
+        console.log(resul);
+        clearDisplay();
+        document.getElementById("display").value = resul;
+}
+
+
+function operacion(operador){
+        
+        if (operador == "+"){
+            memoria = document.getElementById("display").value
+        } else if (operador == "="){
+            //TODO: llamar a calculadora con el valor de la memoria, el display
+        }
+
 }
