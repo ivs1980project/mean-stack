3923a9b164e2645732d6310766f21f6534c91a3e
manana <manana>
2016-11-07 14:00:30 +0100

    practica pelis

--- javascript/Practica01-JQUERY-AJAX/js/pelis-ajax.js
+++ javascript/Practica01-JQUERY-AJAX/js/pelis-ajax.js
@@ -2,16 +2,55 @@ $(document).ready(initializeEvents);
 function initializeEvents(){
     //Cargar la tabla con los datos del json
     peticionAjaxGet();
-    $("#peticion_ajaxPost").click(peticionAjaxPost);
-    $("#peticion_ajaxGet").click(peticionAjaxGet);
-    $("#peticion_ajaxDelete").click(peticionAjaxDelete);
+    $("#save").click(saveNewFilm);
+    $("#modify").click(peticionAjaxGet);
+    $("#delete").click(deleteFilm);
     $("#fecha").datepicker();
 }
 
 function selectRow(){
-    $(this).parent().addClass('row-selected');
+    //Eliminamos las anteriores clases seleccionadas
+    clearRowSelected();
+    $(this).addClass('row-selected');
+    showInForm($('.row-selected').find('.td-titulo').text(),$('.row-selected').find('.td-director').text(),
+        $('.row-selected').find('.td-sinopsis').text(),$('.row-selected').find('.td-fecha').text());
+    
 }
 
+function clearRowSelected(){
+    $('#table-body .film-row').each(function(){
+        $(this).removeClass('row-selected');
+    });
+}
+
+function saveNewFilm(){
+    peticionAjaxPost();
+    reloadTableBody();
+    peticionAjaxGet();
+}
+
+function modifyFilm(){
+    
+}
+
+function deleteFilm(){
+    //Recuperamos los ids de las filas que están seleccionadas para un borrado multiple
+ /*   let rowsToDelete=[];
+    $('#table-body .film-row .td-checked').each(function(){
+        if ($(this)[0].firstChild.checked){
+               
+            }
+        });
+    //TODO:Recuperar el id de la pelicula seleccionada para borrar que estará en el formulario
+    */
+    for (let contador =10; contador <15; contador ++){
+        peticionAjaxDelete(contador);
+        reloadTableBody();
+        peticionAjaxGet();
+    }
+        reloadTableBody();
+        peticionAjaxGet();
+}
 
 function peticionAjaxPut(){
 
@@ -59,15 +98,7 @@ function peticionAjaxGet(){
     }).done(peticionCompletada).fail(peticionFallida);
 }
 
-function peticionAjaxDelete(){
-    //Recuperamos los ids de las filas que están seleccionadas para un borrado multiple
-    let rowsToDelete=[];
-    $('#table-body .film-row .td-checked').each(function(){
-        if ($(this)[0].firstChild.checked){
-               $(this).parent().remove();
-            }
-        });
-    //TODO:Recuperar el id de la pelicula seleccionada para borrar que estará en el formulario
+function peticionAjaxDelete(number){
     $.ajax({
         //Puede ser una cadena, un array o un object de JS
         data: {},
@@ -82,14 +113,15 @@ function peticionAjaxDelete(){
 }
 
 function peticionCompletada(data, status){
-    alert("Peticion completada con status : " +status + ": "+data);
+    //alert("Peticion completada con status : " +status + ": "+data);
     reloadTableBody();
     jsonToRowData(data);
 
 }
 
+
 function peticionFallida(jqXHR, status, error){
-    alert("Error al procesar la peticion");
+    //alert("Error al procesar la peticion");
     console.log("Status " +status);
     console.log("Error! " +error);
 }
@@ -121,3 +153,13 @@ function reloadTableBody(){
     $('#table-body').children().remove();
 }
 
+function showInForm(titulo,director,sinopsis,fecha){
+    $(".form-control#titulo").val(titulo);
+    $(".form-control#director").val(director);
+    $(".form-control#sinopsis").val(sinopsis);
+    $(".form-control#fecha").val(fecha);
+ //   $("#titulo.form-control").attr("value",titulo);
+ //   $("#director.form-control").attr("value",director);
+ //   $("#sinopsis.form-control").attr("value", sinopsis);
+ //  $("#fecha.form-control").attr("value",fecha);
+}
