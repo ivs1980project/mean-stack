b6a22f67810fd4687f145c207c97027d4b6e907f
manana <manana>
2016-12-07 13:05:15 +0100

    Ejemplo Chat angular2

---
+++ javascript/ejemplo10-socketio/socketio-angular2/src/app/message.ts
@@ -0,0 +1,9 @@
+export class Message {
+    constructor(
+        private user: string,
+        private content: string
+    ){}
+    toString(){
+        return this.user+" "+ this.content;
+    }
+}
--- javascript/ejemplo10-socketio/socketio-server/server.js
+++ javascript/ejemplo10-socketio/socketio-server/server.js
@@ -1,23 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//io.origins('*:*');
-//Sólo dejamos conectar a los clientes con las siguientes direcciones
-//io.origins('http://localhost:4200 http://localhost:3200');
+var sockets = [];
 io.origins('http://localhost:4200');
-//Por defecto tenemos ya namespace
-//http://localhost:3000/socket.io/?
-io.on('connection', (socket) => {
-    console.log("Cliente conectado!!!");
-    io.on('mando-un-mensaje', (mensaje) => {
-        console.log("Mensaje recibido: " + mensaje);
-        socket.emit('mando-un-mensaje', mensaje); //yo. Es un eco
-        //io.emit son todos los clientes conectados a este servidor
-        io.emit('mando-un-mensaje', mensaje);
-        //Se envía a todos menos el que lo envía
-        socket.broadcast.emit('mando-un-mensaje', mensaje);
+// Por defecto tenemos ya namespace
+// http://localhost:3000/socket.io
+
+var chat = io.of("/chat");
+chat.on('connection', (socket) => {
+    console.log("Cliente conectado!! Id:" + socket.id);
+    sockets.push(socket);
+    console.log(sockets);
+    socket.on('mando-un-mensaje', (mensaje) => {
+        console.log("Mensaje recibido : ", mensaje);
+
+
+        mensaje.user = socket.id;
+        if (sockets.length > 3) {
+            sockets[3].emit('mando-un-mensaje', { user: "Tu mismo", content: "El contenido que quieras" })
+        }
+        socket.emit('mando-un-mensaje', mensaje); // yo
+        //chat.emit('mando-un-mensaje', mensaje);// a todos
+        //socket.broadcast.emit('mando-un-mensaje', mensaje);// todos menos yo!
     });
 });
-io.on('disconnect', (socket) => {
-    console.log("Cliente desconectado!!!");
+chat.on('disconnect', (socket) => {
+    console.log("Cliente desconectado!!")
 });
+http.listen(3000, () => {
+    console.log("Iniciado en *:3000");
+})
